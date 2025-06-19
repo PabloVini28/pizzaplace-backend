f42ac53878229edca5d621da8a3de0274dcfa714
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-06-19 16:13:38 -0300

    refactor: adding the username at register user

--- src/main/java/com/pardalpizzaria/pizzaria/auth/dtos/request/RegisterUserDto.java
+++ src/main/java/com/pardalpizzaria/pizzaria/auth/dtos/request/RegisterUserDto.java
@@ -2,6 +2,7 @@ package com.pardalpizzaria.pizzaria.auth.dtos.request;
 
 public record RegisterUserDto(
     String name,
+    String username,
     String email,
     String password,
     String confirmPassword,
