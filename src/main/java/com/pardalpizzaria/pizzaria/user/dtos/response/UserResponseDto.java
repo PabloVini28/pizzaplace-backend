4514a08a64e9c6df2e329c7f43b6777de3333cfe
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-06-19 14:49:29 -0300

    refactor: adding the constructor and the id at the response

--- src/main/java/com/pardalpizzaria/pizzaria/user/dtos/response/UserResponseDto.java
+++ src/main/java/com/pardalpizzaria/pizzaria/user/dtos/response/UserResponseDto.java
@@ -1,9 +1,21 @@
 package com.pardalpizzaria.pizzaria.user.dtos.response;
 
+import com.pardalpizzaria.pizzaria.user.entity.User;
+
 public record UserResponseDto(
+    Long id,
     String name,
     String username,
     String email
 ) {
+
+    public UserResponseDto(User user) {
+        this(
+            user.getId(),
+            user.getName(),
+            user.getUsername(),
+            user.getEmail()
+        );
+    }
     
 }
